74aba1e146edf944ec8ae37f0a0a8ffdc1d201a0
Sobia Ahmad <sobia_ahmad_94@outlook.com>
2020-08-22 11:09:56 +0100

    Build Product gallery Adds new assets, components and styling for Product gallery.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app" @mousemove="mousemove">
+    <section class="products">
+      <Product v-for="product in products" :key="product.color" :product="product" />
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from "./components/Product";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    Product,
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: "VANS - Burgundy",
+          color: "blue",
+          bgtext: "OFF VANS",
+          src: require("./assets/vans-trainers-1.png"),
+        },
+        {
+          title: "VANS - Funky Colours",
+          color: "purple",
+          bgtext: "THE VANS",
+          src: require("./assets/vans-trainers-2.png"),
+        },
+        {
+          title: "VANS - Black",
+          color: "green",
+          bgtext: "WALL VANS",
+          src: require("./assets/vans-trainers-3.png"),
+        },
+      ],
+    };
+  },
+  methods: {
+    mousemove(e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let products = document.querySelectorAll(".products .product");
+
+      for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+
+        let product_photo = product.querySelector(".product-photo-wrapper");
+
+        let photo_x = mouseX - this.coords(product_photo).x;
+        let photo_y = mouseY - this.coords(product_photo).y;
+        product_photo.style.transform = `translateY(-${
+          photo_y / 20
+        }px) translateX(-${photo_x / 20}px) translateZ(100px)`;
+
+        let bginfo = product.querySelector(".bg-info");
+        let bg_x = mouseX - this.coords(bginfo).x;
+        let bg_y = mouseY - this.coords(bginfo).y;
+
+        bginfo.style.transform = `translateX(${bg_x})px translateY(${bg_y})px`;
+      }
+    },
+    coords(el) {
+      let coords = el.getBoundingClientRect();
+
+      return {
+        x: coords.left / 2,
+        y: coords.top / 2,
+      };
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: "Open Sans Condensed", sans-serif;
+}
+
+main {
+  background-color: rgb(243, 243, 243);
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100vw;
+}
+
+.products {
+  max-width: 1280px;
+  padding: 25px;
+  margin: 0 auto;
+  display: flex;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="product">
+    <div :class=" 'product-inner ' + product.color">
+      <div class="product-info-wrapper">
+        <h2 class="bg-info">{{ product.bgtext }}</h2>
+      </div>
+      <div class="product-photo-wrapper">
+        <img :src="product.src" class="photo" />
+      </div>
+      <div class="product-description">
+        <h2>{{ product.title }}</h2>
+        <p>"1966 Brothers Paul Van Doren and Jim Van Doren along with partners Gordon Lee and Serge Delia open The Van Doren Rubber Company, Anaheim, Calif., for business on March 16. Vans #44 deck shoes, now known as the Authentic, are born."</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product",
+  props: ["product"],
+};
+</script>
+
+<style>
+.product {
+  width: 100%;
+  flex: 1 1 33.333%;
+  padding: 25px;
+}
+
+.product-inner {
+  position: relative;
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.73);
+  padding: 25px;
+}
+
+.product-inner.blue {
+  background-image: linear-gradient(to bottom left, #24b1d4, #1aa5cf);
+}
+
+.product-inner.purple {
+  background-image: linear-gradient(to bottom right, #c97eec, #93419b 70%);
+}
+
+.product-inner.green {
+  background-image: linear-gradient(to bottom right, #c01e1e, #eb488c 70%);
+}
+
+.product-info-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 0;
+  overflow: hidden;
+}
+
+.product-info-wrapper h2 {
+  color: #313131;
+  font-size: 155px;
+  font-weight: 900;
+  opacity: 0.2;
+  transform-origin: center;
+}
+
+.product-photo-wrapper {
+  z-index: 1;
+  position: relative;
+  transform-origin: center;
+}
+
+.product-photo-wrapper .photo {
+  filter: drop-shadow(0px 0px 0px 12px rgba(0, 0, 0, 0.73));
+  width: 100%;
+}
+
+.product-description {
+  margin: 0px -25px -25px;
+  padding: 25px;
+  background-color: rgb(248, 244, 244);
+}
+
+.product-description h2 {
+  color: rgb(121, 116, 116);
+  text-align: center;
+  font-size: 24px;
+  font-weight: 700;
+  margin-bottom: 15px;
+}
+
+.product-description p {
+  color: rgb(114, 101, 101);
+  line-height: 1.5;
+  font-size: 15px;
+  font-weight: 300;
+}
+</style>
